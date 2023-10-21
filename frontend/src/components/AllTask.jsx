e4cce05866db1c61cc79af1ea11e182c534831f4
Jay Desai <jaydesai571@gmail.com>
2023-10-21 10:48:26 -0400

    Update timestamp
    fetch timestamp and used in frontend.

--- frontend/src/components/AllTask.jsx
+++ frontend/src/components/AllTask.jsx
@@ -3,26 +3,25 @@ import Task from './Task/Task';
 import { useContext } from 'react';
 import TaskContext from '../context/TaskContext';
 function AllTask() {
-    const { tasks } = useContext(TaskContext);
-    return (
-        <div>
-            {
-                (tasks.length !==0) ? (
-                    tasks.map((task, index) => {
-                        return (
-                            <Task
-                                key={index}
-                                task={task}
-                                id={index}
-                            />
-                        )
-                    })
-                ) : (
-                    <h1>No Task Found</h1>
-                )
-            }
-        </div>
-    );
+  const { tasks } = useContext(TaskContext);
+  return (
+    <div>
+      {tasks.length !== 0 ? (
+        tasks.map((task, index) => {
+          return <Task key={index} task={task} id={index} />;
+        })
+      ) : (
+        <h1>No Task Found</h1>
+      )}
+      {tasks.length !== 0 ? (
+        tasks.map((task, index) => {
+          return <Task key={index} task={task} id={index} />;
+        })
+      ) : (
+        <h1>No Task Found</h1>
+      )}
+    </div>
+  );
 }
 
 export default AllTask;
